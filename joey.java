f37c1e4062575249c9edd33d0bf1e4a3f0413c8a
pefa2020 <percyflores.edu@gmail.com>
2024-02-07 22:44:06 -0500

    Success

---
+++ joey.java
@@ -0,0 +1,10 @@
+class Joey {
+    public static void main(String[] args) {
+        // Prints random numnbers
+        System.out.println(1);
+        System.out.println(7);
+        System.out.println(3);
+        System.out.println(4);
+        return;
+    }
+}
