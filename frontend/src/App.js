3f3021e6d2dbed83c879b2e1600a093dca8e76ce
Vikas Shah <114889289+VikShah@users.noreply.github.com>
2024-03-15 22:19:43 -0400

    Testing

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/Header';
+import FilterBar from './components/FilterBar';
+import VehicleGrid from './components/VehicleGrid'; // Make sure this is correctly pointing to where your VehicleGrid component is located.
+import Pagination from './components/Pagination';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <FilterBar />
+      <VehicleGrid /> {/* This replaces the individual VehicleCard component to display a grid of vehicle cards */}
+      <Pagination />
+    </div>
+  );
+}
+
+export default App;
