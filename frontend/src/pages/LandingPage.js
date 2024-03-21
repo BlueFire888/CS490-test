728916377aef4143436a5a78bd9ec9f1500a9e92
krishnaV487 <kvv2@njit.edu>
2024-03-21 10:23:21 -0400

    Navbar modifications and landing page

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Container , Row, Col} from 'react-bootstrap';
 import '../styles/LandingPage.css'
-
+import landingcar from '../images/landing-page-img.jpg'
 function LandingPage(){
 
     return(
+        <div>
+                      <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet'></link>
+
         <Container fluid className='landing'>
             <Row className='landing-top'>
-                <Col sm={8} className='landing-top-l'></Col>
-                <Col className='landing-top-r'></Col>
+                <Col xs={8} className='landing-top-l'></Col>
+                <div className='landing-top-photo'></div>
+                <Container className='landing-top-explore'>
+                    <Row>
+                        <Col className='explore-skyline'>
+                            Skyline Dealership
+                        </Col>
+                    </Row>
+                </Container>
+                <Col xs={4} className='landing-top-r'></Col>
             </Row>
         </Container>
+   
+
+        </div>
     );
 }
 
